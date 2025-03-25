cad7958ef2f2169c98a2bd926d335b289516bbef
oliver <13130886@me.mcu.edu.tw>
2025-03-25 20:57:44 +0800

    feat(ts): 利用類別方法設定產品是否顯示

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
 import { Product } from "./product";
 
-let product = [
+let products = [
   new Product(1, "圖像 Angular 開發入門 第二版", "博碩文化", 500, true, new Date("2024-4-10")),
   new Product(2, "金魚都能懂的 CSS 必學屬性", "博碩文化", 500, true, new Date("2024-4-10")),
 ];
 
-console.table(product);
-setProductDisplay(product[0], false);
+console.table(products);
+products[0].setDisplay(false);
 
-console.table(product);
-
-function setProductDisplay(product: Product, isShow: boolean) {
-  product.isShow = isShow;
-}
+console.table(products);
