9387eff10135dd75381a3c31670d076bdd13e6fd
oliver <13130886@me.mcu.edu.tw>
2025-05-13 01:30:16 +0800

    feat(product): 變更作者資訊為文字陣列

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent {
       new Product({
         id: 1,
         name: '書籍A',
-        authors: '作者甲、作者乙、作者丙',
+        authors: ['作者甲', '作者乙', '作者丙'],
         company: '博碩文化',
 
         isShow: true,
@@ -33,7 +33,7 @@ export class AppComponent {
       new Product({
         id: 2,
         name: '書籍 B',
-        authors: '作者甲、作者乙、作者丙',
+        authors: ['作者甲', '作者乙', '作者丙'],
         company: '博碩文化',
 
         isShow: true,
@@ -47,7 +47,7 @@ export class AppComponent {
       new Product({
         id: 3,
         name: '書籍 C',
-        authors: '作者甲、作者乙、作者丙',
+        authors: ['作者甲', '作者乙', '作者丙'],
         company: '博碩文化',
 
         isShow: true,
@@ -61,7 +61,7 @@ export class AppComponent {
       new Product({
         id: 4,
         name: '書籍 D',
-        authors: '作者甲、作者乙、作者丙',
+        authors: ['作者甲', '作者乙', '作者丙'],
         company: '博碩文化',
 
         isShow: true,
@@ -75,7 +75,7 @@ export class AppComponent {
       new Product({
         id: 5,
         name: '書籍 E',
-        authors: '作者甲、作者乙、作者丙',
+        authors: ['作者甲', '作者乙', '作者丙'],
         company: '博碩文化',
 
         isShow: true,
