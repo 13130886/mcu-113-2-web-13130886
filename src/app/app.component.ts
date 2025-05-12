04898ba52321332b31e88734d23b0f33c943373f
oliver <13130886@me.mcu.edu.tw>
2025-05-13 01:12:18 +0800

    feat(product): 顯示無產品資料訊息實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,76 +9,83 @@ import { Product } from './models/product';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  products = [
-    new Product({
-      id: 1,
-      name: '書籍A',
-      authors: '作者甲、作者乙、作者丙',
-      company: '博碩文化',
+  products: Product[] = [];
 
-      isShow: true,
+  setEmptyDate(): void {
+    this.products = [];
+  }
+  setHasDate(): void {
+    this.products = [
+      new Product({
+        id: 1,
+        name: '書籍A',
+        authors: '作者甲、作者乙、作者丙',
+        company: '博碩文化',
 
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+        isShow: true,
 
-      createDate: new Date('2025/4/9'),
+        photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
 
-      price: 10000,
-    }),
-    new Product({
-      id: 2,
-      name: '書籍 B',
-      authors: '作者甲、作者乙、作者丙',
-      company: '博碩文化',
+        createDate: new Date('2025/4/9'),
 
-      isShow: true,
+        price: 10000,
+      }),
+      new Product({
+        id: 2,
+        name: '書籍 B',
+        authors: '作者甲、作者乙、作者丙',
+        company: '博碩文化',
 
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+        isShow: true,
 
-      createDate: new Date('2025/4/9'),
+        photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
 
-      price: 10000,
-    }),
-    new Product({
-      id: 3,
-      name: '書籍 C',
-      authors: '作者甲、作者乙、作者丙',
-      company: '博碩文化',
+        createDate: new Date('2025/4/9'),
 
-      isShow: true,
+        price: 10000,
+      }),
+      new Product({
+        id: 3,
+        name: '書籍 C',
+        authors: '作者甲、作者乙、作者丙',
+        company: '博碩文化',
 
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+        isShow: true,
 
-      createDate: new Date('2025/4/9'),
+        photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
 
-      price: 10000,
-    }),
-    new Product({
-      id: 4,
-      name: '書籍 D',
-      authors: '作者甲、作者乙、作者丙',
-      company: '博碩文化',
+        createDate: new Date('2025/4/9'),
 
-      isShow: true,
+        price: 10000,
+      }),
+      new Product({
+        id: 4,
+        name: '書籍 D',
+        authors: '作者甲、作者乙、作者丙',
+        company: '博碩文化',
 
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+        isShow: true,
 
-      createDate: new Date('2025/4/9'),
+        photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
 
-      price: 10000,
-    }),
-    new Product({
-      id: 5,
-      name: '書籍 E',
-      authors: '作者甲、作者乙、作者丙',
-      company: '博碩文化',
+        createDate: new Date('2025/4/9'),
 
-      isShow: true,
+        price: 10000,
+      }),
+      new Product({
+        id: 5,
+        name: '書籍 E',
+        authors: '作者甲、作者乙、作者丙',
+        company: '博碩文化',
 
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+        isShow: true,
 
-      createDate: new Date('2025/4/9'),
+        photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
 
-      price: 10000,
-    }),
-  ];
+        createDate: new Date('2025/4/9'),
+
+        price: 10000,
+      }),
+    ];
+  }
 }
